c1240e23afb656be9d0ecb61fd5d87d9022259c1
JuicyTomato <JuicyTomato@users.noreply.github.com>
2024-02-16 17:17:48 +0100

    aggiunta data del giorno e di domani nei quadranti. Aggiunto il movimento del bulb (val destinationX = button.x + (button.width / 2) - (bulbImageView.width / 2) - 512), il 512 non è bellissimo, valuta di vambiare per non rischiare che su diversi dispositivi ci siano problemi. Aggiunti anche i fonts

--- app/src/main/java/com/example/planeat/MainActivity.kt
+++ app/src/main/java/com/example/planeat/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.example.planeat
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Typeface
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+
+    //per la funzione MoveBulbToButton
+    lateinit var bulbImageView: ImageView
+
+    @SuppressLint("SetTextI18n", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //OGGI TEXT VIEW
+        val dateTextView: TextView = findViewById(R.id.dateTextView)
+        //formato data
+        val dateFormat = SimpleDateFormat("MMM d", Locale.getDefault())
+        val dayOfWeekFormat = SimpleDateFormat("EEEE", Locale.getDefault())
+        val currentDate = Date()    //data di oggi
+        //metti nella text view
+        dateTextView.text = dateFormat.format(currentDate).uppercase(Locale.getDefault()) + "\n${dayOfWeekFormat.format(currentDate)}"
+
+
+        //DOMANI TEXTVIEW
+        val tomorrowDateTextView: TextView = findViewById(R.id.tomorrowDateTextView)
+        //ottieni giorno di domani
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_YEAR, 1) // Aggiungi un giorno
+        val tomorrowDate = calendar.time
+        tomorrowDateTextView.text = dateFormat.format(tomorrowDate).uppercase(Locale.getDefault()) + "\n${dayOfWeekFormat.format(tomorrowDate)}"
+
+        //movimento bulb sui bottoni
+        bulbImageView = findViewById(R.id.bulb)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+
+        //per spostare sopra bulb ai bottoni
+        button1.setOnClickListener {
+            moveBulbToButton(button1)
+        }
+        button2.setOnClickListener {
+            moveBulbToButton(button2)
+        }
+        button3.setOnClickListener {
+            moveBulbToButton(button3)
+        }
+        button4.setOnClickListener {
+            moveBulbToButton(button4)
+        }
+
+
+
+    }
+    //funzione per spostare bulb sopra ai bottoni
+    private fun moveBulbToButton(button: Button) {
+        //il 512, penso vada cambiato in base al dispositivo che si utilizza... (anche se aumenti lunghezza long_square)
+        val destinationX = button.x + (button.width / 2) - (bulbImageView.width / 2) - 512
+        val animator = ObjectAnimator.ofFloat(bulbImageView, "translationX", destinationX)
+        animator.duration = 500
+        animator.start()
     }
 }
