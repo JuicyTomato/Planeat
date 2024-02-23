a85c776500a53b9b54d6f80f1936a2b77f99aa59
JuicyTomato <JuicyTomato@users.noreply.github.com>
2024-02-23 23:37:26 +0100

    Sistemato PlanYourEats e la creazione dei bottoni e loro eventi

--- app/src/main/java/com/example/planeat/PlanYourEats.kt
+++ app/src/main/java/com/example/planeat/PlanYourEats.kt
@@ -4,9 +4,12 @@ import android.annotation.SuppressLint
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.GridLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
@@ -24,6 +27,11 @@ class PlanYourEats: AppCompatActivity() {
         setContentView(R.layout.plan_your_eats)
 
 
+
+//PER ROOM AGGIUNGI ALLA DATA L'ANNO, FAI GETTEXT() + YEAR
+
+
+
         buttonNext = findViewById<Button>(R.id.buttonNext)
         buttonPreview = findViewById<Button>(R.id.buttonPreview)
         val backButton = findViewById<Button>(R.id.backButton)
@@ -33,64 +41,82 @@ class PlanYourEats: AppCompatActivity() {
         //formato data
         val oneCalendar = Calendar.getInstance()
 
-        val oneTextView: TextView = findViewById(R.id.date1)
         val oneDate = oneCalendar.time
 
         //+1
         val twoCalendar = Calendar.getInstance()
-        val twoTextView: TextView = findViewById(R.id.date2)
         val twoDate = twoCalendar.time
 
         //+2
         val threeCalendar = Calendar.getInstance()
-        val threeTextView: TextView = findViewById(R.id.date3)
         val threeDate = threeCalendar.time
 
         //+3
         val fourCalendar = Calendar.getInstance()
-        val fourTextView: TextView = findViewById(R.id.date4)
         val fourDate = fourCalendar.time
 
         //+4
         val fiveCalendar = Calendar.getInstance()
-        val fiveTextView: TextView = findViewById(R.id.date5)
         val fiveDate = fiveCalendar.time
 
         //+5
         val sixCalendar = Calendar.getInstance()
-        val sixTextView: TextView = findViewById(R.id.date6)
         val sixDate = sixCalendar.time
 
         //+6
         val sevenCalendar = Calendar.getInstance()
-        val sevenTextView: TextView = findViewById(R.id.date7)
         val sevenDate = sevenCalendar.time
 
         //+7
         val eightCalendar = Calendar.getInstance()
-        val eightTextView: TextView = findViewById(R.id.date8)
         val eightDate = eightCalendar.time
 
-        //I
-        updateTextView(oneTextView, oneCalendar, oneDate, 0)
-        //II
-        updateTextView(twoTextView, twoCalendar, twoDate, 1)
-        //III
-        updateTextView(threeTextView, threeCalendar, threeDate, 2)
-        //IV
-        updateTextView(fourTextView, fourCalendar, fourDate, 3)
-        //V
-        updateTextView(fiveTextView, fiveCalendar, fiveDate, 4)
-        //VI
-        updateTextView(sixTextView, sixCalendar, sixDate, 5)
-        //VII
-        updateTextView(sevenTextView, sevenCalendar, sevenDate, 6)
-        //VIII
-        updateTextView(eightTextView, eightCalendar, eightDate, 7)
-
-        //barra sopra
+        //per barra sopra
+        val lastCalendar = Calendar.getInstance()
+        val lastDate = eightCalendar.time
+        lastCalendar.add(Calendar.DAY_OF_YEAR, 7)
         val dateViewText: TextView = findViewById(R.id.dateView)
-        updateDateViewText(dateViewText, oneCalendar, eightCalendar)
+        updateDateViewText(dateViewText, oneCalendar, lastCalendar)
+
+
+
+        val gridLayout = findViewById<GridLayout>(R.id.grillNotGrid)
+        val button = Button(this)
+        createButtons(button, updateTextView(oneCalendar, 0))
+        gridLayout.addView(button)
+        button.setOnClickListener{
+            //robe
+        }
+
+        val button2 = Button(this)
+        createButtons(button2, updateTextView(twoCalendar, 1))
+        gridLayout.addView(button2)
+
+        val button3 = Button(this)
+        createButtons(button3, updateTextView(threeCalendar, 2))
+        gridLayout.addView(button3)
+
+        val button4 = Button(this)
+        createButtons(button4, updateTextView(fourCalendar, 3))
+        gridLayout.addView(button4)
+
+        val button5 = Button(this)
+        createButtons(button5, updateTextView(fiveCalendar, 4))
+        gridLayout.addView(button5)
+
+        val button6 = Button(this)
+        createButtons(button6, updateTextView(sixCalendar, 5))
+        gridLayout.addView(button6)
+
+        val button7 = Button(this)
+        createButtons(button7, updateTextView(sevenCalendar, 6))
+        gridLayout.addView(button7)
+
+        val button8 = Button(this)
+        createButtons(button8, updateTextView(eightCalendar, 7))
+        gridLayout.addView(button8)
+
+
 
         //Bottone per passare ai prossimi 8 giorni
         buttonNext.setOnClickListener {
@@ -103,21 +129,21 @@ class PlanYourEats: AppCompatActivity() {
             */
 
             //I
-            updateTextView(oneTextView, oneCalendar, oneDate, contDate)
+            createButtons(button, updateTextView(oneCalendar, contDate))
             //II
-            updateTextView(twoTextView, twoCalendar, twoDate, contDate)
+            createButtons(button2, updateTextView(twoCalendar, contDate))
             //III
-            updateTextView(threeTextView, threeCalendar, threeDate, contDate)
+            createButtons(button3, updateTextView(threeCalendar, contDate))
             //IV
-            updateTextView(fourTextView, fourCalendar, fourDate, contDate)
+            createButtons(button4, updateTextView(fourCalendar, contDate))
             //V
-            updateTextView(fiveTextView, fiveCalendar, fiveDate, contDate)
+            createButtons(button5, updateTextView(fiveCalendar, contDate))
             //VI
-            updateTextView(sixTextView, sixCalendar, sixDate, contDate)
+            createButtons(button6, updateTextView(sixCalendar, contDate))
             //VII
-            updateTextView(sevenTextView, sevenCalendar, sevenDate, contDate)
+            createButtons(button7, updateTextView(sevenCalendar, contDate))
             //VIII
-            updateTextView(eightTextView, eightCalendar, eightDate, contDate)
+            createButtons(button8, updateTextView(eightCalendar, contDate))
 
             //barra sopra
             updateDateViewText(dateViewText, oneCalendar, eightCalendar)
@@ -125,26 +151,29 @@ class PlanYourEats: AppCompatActivity() {
         //Bottone per tornare indietro agli scorsi 8 giorni
         buttonPreview.setOnClickListener {
             //I
-            updateTextView(oneTextView, oneCalendar, oneDate, -contDate)
+            createButtons(button, updateTextView(oneCalendar, -contDate))
             //II
-            updateTextView(twoTextView, twoCalendar, twoDate, -contDate)
+            createButtons(button2, updateTextView(twoCalendar, -contDate))
             //III
-            updateTextView(threeTextView, threeCalendar, threeDate, -contDate)
+            createButtons(button3, updateTextView(threeCalendar, -contDate))
             //IV
-            updateTextView(fourTextView, fourCalendar, fourDate, -contDate)
+            createButtons(button4, updateTextView(fourCalendar, -contDate))
             //V
-            updateTextView(fiveTextView, fiveCalendar, fiveDate, -contDate)
+            createButtons(button5, updateTextView(fiveCalendar, -contDate))
             //VI
-            updateTextView(sixTextView, sixCalendar, sixDate, -contDate)
+            createButtons(button6, updateTextView(sixCalendar, -contDate))
             //VII
-            updateTextView(sevenTextView, sevenCalendar, sevenDate, -contDate)
+            createButtons(button7, updateTextView(sevenCalendar, -contDate))
             //VIII
-            updateTextView(eightTextView, eightCalendar, eightDate, -contDate)
+            createButtons(button8, updateTextView(eightCalendar, -contDate))
 
             //barra sopra
             updateDateViewText(dateViewText, oneCalendar, eightCalendar)
 
         }
+
+
+
 
         //torna alla MainActivity
         backButton.setOnClickListener {
@@ -154,19 +183,31 @@ class PlanYourEats: AppCompatActivity() {
 
     }
 
+    private fun createButtons(button: Button, text: String){
+        button.id = View.generateViewId()
+        button.layoutParams = GridLayout.LayoutParams().apply {
+            width = GridLayout.LayoutParams.WRAP_CONTENT
+            height = GridLayout.LayoutParams.WRAP_CONTENT
+            columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f) // Occupa una colonna
+            rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+            setMargins(25, 25, 25, 25) // Imposta i margini del pulsante
+        }
+        val icon = ContextCompat.getDrawable(this, R.drawable.default_square)
+        button.background = icon
+        button.text = text
+    }
+
     @SuppressLint("SetTextI18n")
     private fun updateTextView(
-        textView: TextView,
         calendar: Calendar,
-        date: Date,
         contDate: Int
-    ) {
+    ): String {
         calendar.add(Calendar.DAY_OF_YEAR, contDate)
         val dateFormat = SimpleDateFormat("MMM d", Locale.getDefault())
         val dayOfWeekFormat = SimpleDateFormat("EEEE", Locale.getDefault())
         val formattedDate = dateFormat.format(calendar.time).uppercase(Locale.getDefault())
         val dayOfWeek = dayOfWeekFormat.format(calendar.time)
-        textView.text = "$formattedDate\n$dayOfWeek"
+        return "$formattedDate\n$dayOfWeek"
     }
 
     fun updateDateViewText(
